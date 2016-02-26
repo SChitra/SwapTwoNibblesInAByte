ca4f2d6e17fccb35354feba84733763a52f5bb8f
C S <sethuchitra@live.com>
2016-02-26 14:28:42 -0800

    Swap Two Nibbles in a Byte.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    //Swap two nibbles in a byte
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = x << 4| x >> 4;
+            
+            Console.WriteLine("Original number {0}, Swapped number {1}",x,y);
+        }
+    }
+}
